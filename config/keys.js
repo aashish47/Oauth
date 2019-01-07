28fb7d71a5d2011d06de2527e429491ff5d5862b
Aashish Agarwal <aashish27nov@gmail.com>
2019-01-08 00:03:58 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports = {
+    google:{
+        clientID:'686497152271-88sa90j9tiejahs7hbeggfe7had8nji0.apps.googleusercontent.com',
+        clientSecret:'B38neG53ODUMsn-ToWxeIzp3'
+    },
+    mongodb:{
+        dbURI:'mongodb://aashish:Allstar100**@ds211613.mlab.com:11613/nn-oauth-test'
+    },
+    session:{
+        cookieKey:"thenetninjaisawesomeiguess"
+    }
+};
